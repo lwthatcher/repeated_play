f2036c9e18858fe498faf2f05fd23b7d432189b1
Lawrence Thatcher <lwthatcher@msn.com>
2016-11-23 19:54:36 -0700

    run() function now takes in an optional number-of-runs argument

--- src/tournament.py
+++ src/tournament.py
@@ -13,21 +13,24 @@ class Tournament:
         self.trials = trials
         self.p = p
 
-    def run(self):
-        results = self.results_map()
-        for a1 in self.AGENTS:
-            for a2 in self.AGENTS:
-                agent1 = get_agent(a1)
-                agent2 = get_agent(a2)
-                r = self.game.run(agent1, agent2)
-                results[a1][a2] = r[0]
-        self.export(results)
+    def run(self,num_times=1):
+        for t in range(num_times):
+            results = self.results_map()
+            for a1 in self.AGENTS:
+                for a2 in self.AGENTS:
+                    agent1 = get_agent(a1)
+                    agent2 = get_agent(a2)
+                    r = self.game.run(agent1, agent2)
+                    results[a1][a2] = r[0]
+            self.export(results, t+1)
 
-    def export(self, results):
+    def export(self, results, t):
         with open(self.output_file, 'a') as csv_file:
             fieldnames = self.AGENTS
             fieldnames.insert(0, 'name')
             print("fieldnames", fieldnames)
+            writer = csv.writer(csv_file)
+            writer.writerow([t])
             writer = csv.DictWriter(csv_file, fieldnames=fieldnames)
             writer.writeheader()
             print('AGENTS', self.AGENTS)
@@ -62,7 +65,8 @@ if __name__ == '__main__':
     parser = argparse.ArgumentParser()
     parser.add_argument('trials', type=int, nargs='?', default=100, help='the number of repeat trials to run')
     parser.add_argument('-p', type=float, default=1.0, help='the probability of continuing to another round')
+    parser.add_argument('--runs', '-r', type=int, default=1, help='the number of consecutive runs to do')
     args = parser.parse_args()
 
     tournament = Tournament(args.trials, args.p)
-    tournament.run()
+    tournament.run(args.runs)
