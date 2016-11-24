0501a3a7b52ff9a242845cb28473c7f926e1399b
Lawrence Thatcher <lwthatcher@msn.com>
2016-11-23 17:11:32 -0700

    tournament now has property for getting the appropriate output file destination

--- src/tournament.py
+++ src/tournament.py
@@ -2,11 +2,16 @@ import argparse
 import csv
 from prisoners_dillema import *
 
+
 class Tournament:
     OUTPUT_FILE = 'results.csv'
+    OUTPUT_DIR = '../results/'
+    EXTENSION = '.csv'
 
     def __init__(self, trials=100, p=1.0):
         self.game = RepeatedPlay(trials, p)
+        self.trials = trials
+        self.p = p
 
     def run(self):
         results = self.results_map()
@@ -19,7 +24,7 @@ class Tournament:
         self.export(results)
 
     def export(self, results):
-        with open(self.OUTPUT_FILE, 'a') as csv_file:
+        with open(self.output_file, 'a') as csv_file:
             fieldnames = self.AGENTS
             fieldnames.insert(0, 'name')
             print("fieldnames", fieldnames)
@@ -28,6 +33,19 @@ class Tournament:
             print('AGENTS', self.AGENTS)
             for name in self.AGENTS:
                 writer.writerow(results[name])
+
+    @property
+    def output_file(self):
+        return self.OUTPUT_DIR + self.num_trials + '_' + self.gamma + self.EXTENSION
+
+    @property
+    def num_trials(self):
+        return str(self.trials).zfill(3)
+
+    @property
+    def gamma(self):
+        result = '{:4.2f}'.format(self.p)
+        return result.replace('.', '')
 
     @property
     def AGENTS(self):
@@ -43,8 +61,9 @@ class Tournament:
 if __name__ == '__main__':
     parser = argparse.ArgumentParser()
     parser.add_argument('trials', type=int, nargs='?', default=100, help='the number of repeat trials to run')
-    parser.add_argument('--p', type=float, default=1.0, help='the probability of continuing to another round')
+    parser.add_argument('-p', type=float, default=1.0, help='the probability of continuing to another round')
     args = parser.parse_args()
 
     tournament = Tournament(args.trials, args.p)
-    tournament.run()
+    # tournament.run()
+    print(tournament.output_file)
